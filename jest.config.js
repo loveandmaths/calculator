f62a65ad59f3194cfac52d9748edd469028cb56d
Alexandra Watson <alexandra.watson@hotmail.com>
2025-06-21 19:32:58 +1000

    Added jest and prettier to keep code clean and test driven

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
+export const moduleFileExtensions = ['ts', 'tsx', 'js'];
+export const testMatch = ['**/__tests__/**/*.test.ts'];
