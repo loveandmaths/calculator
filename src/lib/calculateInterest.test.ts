414fe8db5fbe1d605f06800070434055b86e7159
Alexandra Watson <alexandra.watson@hotmail.com>
2025-06-21 21:17:53 +1000

    Added calculations for monthly, quarterly and annual with tests

--- src/lib/calculateInterest.test.ts
+++ src/lib/calculateInterest.test.ts
@@ -12,3 +12,42 @@ describe('CalculateInterest', () => {
     expect(result).toBeCloseTo(10330, 2);
   });
 });
+
+describe('CalculateInterest', () => {
+  it('calculates compound interest monthly', () => {
+    const result = CalculateInterest({
+      principal: 20000,
+      annualRate: 1.8,
+      compounding: 'monthly',
+      years: 4,
+    });
+
+    expect(result).toBeCloseTo(21492, 2);
+  });
+});
+
+describe('CalculateInterest', () => {
+  it('calculates compound interest quarterly', () => {
+    const result = CalculateInterest({
+      principal: 10000,
+      annualRate: 1.1,
+      compounding: 'quarterly',
+      years: 3,
+    });
+
+    expect(result).toBeCloseTo(10335, 2);
+  });
+});
+
+describe('CalculateInterest', () => {
+  it('calculates compound interest annually', () => {
+    const result = CalculateInterest({
+      principal: 10000,
+      annualRate: 1.1,
+      compounding: 'annually',
+      years: 3,
+    });
+
+    expect(result).toBeCloseTo(10334, 4);
+  });
+});
--- src/lib/calculateInterest.ts
+++ src/lib/calculateInterest.ts
@@ -8,11 +8,36 @@ interface CalculatorInput {
 }
 
 export function CalculateInterest(calculatorInput: CalculatorInput) {
-  var result =
-    calculatorInput.principal +
-    calculatorInput.principal *
-      (calculatorInput.annualRate / 100) *
-      calculatorInput.years;
+  const annualRate = calculatorInput.annualRate / 100;
+  const years = calculatorInput.years;
+  const principal = calculatorInput.principal;
+  let result = calculatorInput.principal;
 
-  return result;
+  switch (calculatorInput.compounding) {
+    case 'monthly':
+      const monthlyRate = annualRate / 12;
+      const totalMonths = years * 12;
+      for (let i = 0; i < totalMonths; i++) {
+        result += result * monthlyRate;
+      }
+      break;
+    case 'quarterly':
+      const quarterlyRate = annualRate / 4;
+      const totalQuarters = years * 4;
+      for (let i = 0; i < totalQuarters; i++) {
+        result += result * quarterlyRate;
+      }
+      break;
+    case 'annually':
+      for (let year = 0; year < years; year++) {
+        result += result * annualRate;
+      }
+      break;
+    case 'maturity':
+      result = principal + principal * annualRate * years;
+      break;
+    default:
+      throw new Error('Unknown frequency');
+  }
+  return Math.round(result);
 }
